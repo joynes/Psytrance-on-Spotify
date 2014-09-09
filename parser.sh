d93fe8c5063d0e350700b958e3b04881592170ec
johannes <johannes@spotify.com>
2014-09-09 22:18:24 +0200

    WIP: Replace python parser with bash regexp

---
+++ parser.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+header=`grep '^<TITLE>' $1 | sed 's/<TITLE>\(.*\) (.*/\1/' | sed 's/EP//'`
+artist=${header%:*}
+title=${header#*:}
+echo "Artist: $artist, Title: $title"
+track1=`grep '<B>1\. ' $1 | sed 's/.*<B>1\. \([^<]*\).*/\1/'`
+echo "Track1: $track1"
+
+style=`grep 'Style:' $1  | sed 's/.*Style:[^m]*m">\([^<]*\).*/\1/'`
+echo "Style: $style"
+time=`grep 'Released' $1 | sed 's/.*Released[^m]*m">\([^<]*\).*/\1/'`
+month=${time%.*}
+year=${time%/*}
+year=${year#*. }
+day=${time#*/}
+declare -A montharr
+montharr=([Jan]=1 [Feb]=2 [Mar]=3 [Apr]=4 [May]=5 [Jun]=6 [Jul]=7 [Aug]=8 [Sep]=9 [Okt]=10 [Nov]=11 [Dec]=12)
+monthday=${montharr[Jan]}
+echo "Time: $time"
+echo "Year: $year"
+echo "Month: $month -> $monthday"
+echo "Day: $day"
--- run.sh
+++ run.sh
@@ -1,41 +1,46 @@
+#!/bin/bash
+
 #------------------------------------------------------
-#What are the steps?
+# All the steps
 #------------------------------------------------------
 echo -------------------------------------------
 echo Download all the search pages
 echo -------------------------------------------
 
 year=${1:?"Usage: echo $0 <year>"}
-exit 1
+mkdir -p pages
 cd pages
-mkdir $year
+mkdir -p $year
 cd $year
-exit 1
-i=1; while [ $i -lt 44 ]; do nr=$i; [ $nr -lt 10 ] && nr=0$nr; [ -e $i.html ] || { curl -d "boolean=AND&case=INSENSITIVE&cd=TRUE&dw=TRUE&cs=TRUE&vinyl=TRUE&dvd=TRUE&terms=$year%2F$nr" http://www.psyshop.com/cgi-bin/search.cgi > $i.html; }; let i=i+1; done
+i=1; while [ $i -lt 2 ]; do nr=$i; [ $nr -lt 10 ] && nr=0$nr; [ -e $i.html ] && echo "Psyshop file $i.html already exists" || { \
+curl -H 'Referer: http://www.psyshop.com/' -d 'boolean=AND&case=INSENSITIVE&other=TRUE&cd=TRUE&dw=TRUE&terms='$year'%2F'$nr 'http://www.psyshop.com/psyfctn/psysrch' > $i.html;
+}; let i=i+1; done
 
 echo -------------------------------------------
 echo Get all individual links from theese pages
 echo -------------------------------------------
 
-grep '<TD ALIGN=.*><TABLE><TR><TD ALIGN=.*><A HREF="http://www.psyshop.com' *.html | sed 's/.*"\(ht.*html\)">.*/\1/' > links.txt
+grep '<DIV CLASS="n"><A HREF="/shop/' *.html | sed 's/.*<A HREF="\([^"]*\).*/http:\/\/www.psyshop.com\1/' > links.txt
+
 echo "Found " `cat links.txt | wc | awk '{print $1}'` albums
-mkdir albums
+mkdir -p albums
 cd albums
 
 echo -------------------------------------------
 echo Get all individual pages.. will take time..
 echo -------------------------------------------
 
-i=0; cat ../links.txt |  while read in; do [ -e $i.html ] || { curl $in > $i.html; echo $in > $i.link; }; let i=$i+1; done
+i=0; cat ../links.txt | head -n 1 | while read in; do [ -e $i.html ] && echo "Individual album page $i.html exists..." || { curl $in > $i.html; echo $in > $i.link; }; let i=$i+1; done
 
 
 echo -------------------------------------------
 echo  Parse all the pages and get the inserts
 echo -------------------------------------------
 
-size=`ls | sort -n | grep "\.link$" | tail -n 1 | sed 's/.link//'`
-i=0; while [ $i -lt $size ]; do python ../../../parser.py  $i.html `cat $i.link`; let i=i+1; done  | tee inserts.txt
+size=`ls | grep link | wc -l`
+i=0; while [ $i -lt $size ]; do bash ../../../parser.sh  $i.html `cat $i.link`; let i=i+1; done  | tee inserts.txt
 
+exit 1
 rm data.php
 echo '<?php $array = array(' >> data.php
 cat inserts.txt | grep "^array(" >> data.php
